710abfa9020218326a9d9cff5797770ed17c5132
ivanadepiyo <ivanadepiyo11@gmail.com>
2026-09-13 10:28:30 +0900

    Update firebase-config.js

--- assets/js/firebase/firebase-config.js
+++ assets/js/firebase/firebase-config.js
@@ -1,1 +1,8 @@
-
+export const firebaseConfig = {
+    apiKey: "AIzaSyASi1AG055OektLx3Glg0ALxkkTD6l8Ghs",
+    authDomain: "aku-bisa-46929.firebaseapp.com",
+    projectId: "aku-bisa-46929",
+    storageBucket: "aku-bisa-46929.firebasestorage.app",
+    messagingSenderId: "416383916098",
+    appId: "1:416383916098:web:5d705e8567c9014b25b5db"
+};
