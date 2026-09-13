78a087386212c51ef868d4e58f9853b5c5b01548
ivanadepiyo <ivanadepiyo11@gmail.com>
2026-09-13 10:28:46 +0900

    Update firebase-init.js

--- assets/js/firebase/firebase-init.js
+++ assets/js/firebase/firebase-init.js
@@ -1,1 +1,24 @@
+import { initializeApp } from
+    "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
 
+import { getAuth } from
+    "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
+
+import { getFirestore } from
+    "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
+
+import { firebaseConfig } from "./firebase-config.js";
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+export {
+    app,
+    auth,
+    db
+};
+
+console.log("AKU BISA: Firebase berhasil diinisialisasi.");
